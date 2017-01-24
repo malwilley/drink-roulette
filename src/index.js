48569edd2c40ecec8cd79b21e2e0c5eb55a5fd0a
Malachi Willey <malwilley@gmail.com>
2017-01-23 21:52:30 -0500

    Separated elm and js code into their own folders

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@
 require('ace-css/css/ace.css');
 require('font-awesome/css/font-awesome.css');
 
-require('./Styles/main.scss');
+require('./styles/main.scss');
 require('./index.html');
-var drinkSvg = require('./drinkSvg');
+var drinkSvg = require('./js/drinkSvg');
 
-var Elm = require('./Main.elm');
+var Elm = require('./Elm/Main.elm');
 var mountNode = document.getElementById('main');
 var app = Elm.Main.embed(mountNode);
 
